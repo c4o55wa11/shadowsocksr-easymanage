747374726b5cc9658efda324928f1308c60b16e4
c4o55wa11 <toc0rpse@gmail.com>
2017-07-25 11:46:08 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
-git clone https://github.com/shadowsocksr/shadowsocksr.git
-cd shadowsocksr && ./initcfg.sh && mv user-config.json{,.bak} && cp ../user-config.json ./
-cd .. && python monitor.py 8080 ./shadowsocksr/shadowsocks server.py
+ssr_path='./shadowsocksr'
+if [! -d "$ssr_path"]; then
+    echo '[+] clone SSR project.'
+    git clone https://github.com/shadowsocksr/shadowsocksr.git
+    echo '[+] init configure files.'
+    cd shadowsocksr && ./initcfg.sh 
+    echo '[+] backup old configure file.'
+    mv user-config.json{,.bak}
+    echo '[+] replace configure file.'
+    cp ../user-config.json ./
+    echo '[+] start ssr service and ssr monitor service.'
+    cd .. && python monitor.py 8080 ./shadowsocksr/shadowsocks server.py
+    echo '[+] install finished.Have a good day!'
+else
+    echo '[-]ShadowsocksR project already exists.'
+    echo '[-]If you still want to install or reinstall SSR, pleaser run \'rm -rf ./shadowsocksr \' first.'
+
